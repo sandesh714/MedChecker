93d569ac79d54dfb6ccae2cd64f18d8dec14f28e
Sandesh Lamsal <sandeshlamsal714@gmail.com>
2021-09-06 08:15:17 -0500

    whole lotta changes/user specifics

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from multiselectfield import MultiSelectField
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Schedules(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
     WEEK_DAYS = (
         ('Sunday','Sunday'),
         ('Monday','Monday'),
@@ -16,7 +17,9 @@ class Schedules(models.Model):
     )
 
     medicine_name = models.CharField(max_length=100)
-    Date_From = models.DateField()
-    Date_To = models.DateField()
+    Date_From = models.DateField(default = '')
+    Date_To = models.DateField(default = '')
     weeks = MultiSelectField(choices = WEEK_DAYS)
 
+    def __str__(self):
+        return f'user.username'
