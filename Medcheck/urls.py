346e823b0458ac5fcd97432d1e78112a250ae533
Sandesh Lamsal <sandeshlamsal714@gmail.com>
2021-09-06 05:38:29 -0500

    Added Signup and Login Route

--- Medcheck/urls.py
+++ Medcheck/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from registration import views as registration_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mainapp.urls')),
-    path('signup/',include('registration.urls'))
+    path('signup/', registration_views.signup,name = 'signup'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'registration/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'registration/logout.html'))
+
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,18 +1,19 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegistration
 # Create your views here.
 
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistration(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f"Account created for {username}")
             return redirect('main-home')
     else:
-        form = UserCreationForm()
+        form = UserRegistration()
 
     return render(request, 'registration/signup.html', {'form':form})
 
